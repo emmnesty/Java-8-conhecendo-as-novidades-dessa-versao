b07974b53501104f0f7590707ce693f1e2bd9407
emmnesty <elyjhonatan@hotmail.com>
2022-08-05 13:24:52 -0300

    Classes anonimas, Lambdas

--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -12,41 +12,35 @@ public class OrdenaStrings {
 		palavras.add("editora casa do codigo");
 		palavras.add("caelum");
 
-		Comparator<String> comparador = new ComparadoPorTamanho();
+		// #Primeira forma
+		palavras.sort((s1, s2) -> {
+			if (s1.length() < s2.length())
+				return -1;
+			if (s1.length() > s2.length())
+				return 1;
 
-		// Collections.sort(palavras, comparador);
-		palavras.sort(comparador);
+			return 0;
+		});
+
+		// #Segunda forma
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
 		System.out.println(palavras);
 
 //		for (String p : palavras) {
 //			System.out.println(p);
 //		}
 
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
+		// Com lambda ->
+		palavras.forEach(s -> System.out.println(s));
 
+		// Sem lambda ->
+//		class ImprimeNaLinha implements Consumer<String> {
+//
+//			@Override
+//			public void accept(String s) {
+//
+//			}
+//		}
 	}
-
-}
-
-class ImprimeNaLinha implements Consumer<String> {
-
-	@Override
-	public void accept(String s) {
-
-	}
-}
-
-class ComparadoPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String s1, String s2) {
-		if (s1.length() < s2.length())
-			return -1;
-		if (s1.length() > s2.length())
-			return 1;
-
-		return 0;
-	}
-
 }
